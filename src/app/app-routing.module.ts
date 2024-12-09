4db5cdccf6118ef4f9dc381a8a3622536de20bad
Ayoub Legouirah <ayoubgrand@outlook.fr>
2024-12-09 18:40:10 +0100

    create branch and components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Page1Component } from './pages/page1/page1.component';
+import { Page2Component } from './pages/page2/page2.component';  // Import de la Page2Component
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/page1', pathMatch: 'full' },
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },  // Route pour la Page2
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { Page1Component } from './pages/page1/page1.component';
+import { Page2Component } from './pages/page2/page2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
     BrowserModule,
